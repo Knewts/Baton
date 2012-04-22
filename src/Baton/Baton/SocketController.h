65b1aca3f84eaf414163a6ab4b36213449c1c49f
David Knewtson <matthew.d.knewtson@wmich.edu>
2012-04-21 22:24:27 -0400

    switching OSC Libraries.  VVOSC IS OUT.  WSOSC is in.

---
+++ src/Baton/Baton/SocketController.h
@@ -0,0 +1,13 @@
+//
+//  SocketController.h
+//  Baton
+//
+//  Created by Matthew Knewtson on 4/21/12.
+//  Copyright (c) 2012 Western Michigan University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SocketController : NSObject
+
+@end
---
+++ src/Baton/Baton/SocketController.m
@@ -0,0 +1,13 @@
+//
+//  SocketController.m
+//  Baton
+//
+//  Created by Matthew Knewtson on 4/21/12.
+//  Copyright (c) 2012 Western Michigan University. All rights reserved.
+//
+
+#import "SocketController.h"
+
+@implementation SocketController
+
+@end
