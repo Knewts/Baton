076d85e7187a126f8c1c37aabeaf9a9f5424f486
David Knewtson <matthew.d.knewtson@wmich.edu>
2012-04-23 11:47:46 -0400

    OSC Message class created. Saving before diving in to resolving the spike

---
+++ src/Baton/Baton/OSCMessage.h
@@ -0,0 +1,13 @@
+//
+//  OSCFormater.h
+//  Baton
+//
+//  Created by Matthew Knewtson on 4/23/12.
+//  Copyright (c) 2012 Western Michigan University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OSCFormater : NSObject
+
+@end
---
+++ src/Baton/Baton/OSCMessage.m
@@ -0,0 +1,13 @@
+//
+//  OSCFormater.m
+//  Baton
+//
+//  Created by Matthew Knewtson on 4/23/12.
+//  Copyright (c) 2012 Western Michigan University. All rights reserved.
+//
+
+#import "OSCFormater.h"
+
+@implementation OSCFormater
+
+@end
