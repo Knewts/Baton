524423df81aff11c9e94aee165c17b429b77ce75
David Knewtson <matthew.d.knewtson@wmich.edu>
2012-04-26 12:30:30 -0400

    still working on core.  Added unit tests for OSCstring objects.  Currently has a couple of issues...
    SIGABORT unrecognized selector sent to [__NSCFString sendMessage:toHost:onPort:]
    
    or
    
    EXEC_BAD_ACCESS

---
+++ src/Baton/Baton/OSCstring.m
@@ -0,0 +1,13 @@
+//
+//  OSCstring.m
+//  Baton
+//
+//  Created by Matthew Knewtson on 4/24/12.
+//  Copyright (c) 2012 Western Michigan University. All rights reserved.
+//
+
+#import "OSCstring.h"
+
+@implementation OSCstring
+
+@end
