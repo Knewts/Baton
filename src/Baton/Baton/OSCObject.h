2dd01c98c1b583b740c3d1c460bc7a5aca51d56e
David Knewtson <matthew.d.knewtson@wmich.edu>
2012-05-08 19:19:25 -0400

    Abstracting OSCObject to cover the four object types.  Object types blob and String currently included, Int and Float to follow.

---
+++ src/Baton/Baton/OSCObject.h
@@ -0,0 +1,13 @@
+//
+//  OSCObject.h
+//  Baton
+//
+//  Created by Matthew Knewtson on 5/8/12.
+//  Copyright (c) 2012 Western Michigan University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol OSCObject <NSObject>
+
+@end
