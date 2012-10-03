9cb4c5aa3f8c37e149cc43c3fdb69e345a06dce0
Andy Stratton <stratton99@gmail.com>
2012-10-03 08:24:44 -0400

    Delegate tested and working properly

---
+++ spikes/DataCenterSpike/DataCenterSpike/Baton_UIButton.h
@@ -0,0 +1,13 @@
+//
+//  Baton_UIButton.h
+//  DataCenterSpike
+//
+//  Created by Andy Stratton on 10/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Baton_UIButton : UIView
+
+@end
---
+++ spikes/DataCenterSpike/DataCenterSpike/Baton_UIButton.m
@@ -0,0 +1,13 @@
+//
+//  Baton_UIButton.m
+//  DataCenterSpike
+//
+//  Created by Andy Stratton on 10/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Baton_UIButton.h"
+
+@implementation Baton_UIButton
+
+@end
