248f6a743a9721dac0bb16fcfc078e664ae4e057
David Knewtson <matthew.d.knewtson@wmich.edu>
2012-12-14 11:54:16 -0500

    fixed saving layouts on application termination and resume

--- src/Baton/DataCenterSpike/AppDelegate.m
+++ src/Baton/DataCenterSpike/AppDelegate.m
@@ -69,6 +69,7 @@
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     
     [config writeDefaults];
+    [layouts saveLayoutsToDefaultFile];
 
 }
 
@@ -88,6 +89,7 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     
     [config writeDefaults];
+    [layouts saveLayoutsToDefaultFile];
     
 }
 
